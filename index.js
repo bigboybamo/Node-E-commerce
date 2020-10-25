bcab876e1da5593550f377da9a395468bc2cebc0
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-25 15:44:55 +0100

    added mocha and chai for testing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = express()
 
 app.get('/', (req, res) => {
@@ -9,3 +8,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
+
+module.exports = app
