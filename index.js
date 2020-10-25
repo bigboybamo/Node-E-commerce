2f24d5475dc5daf9c16b2e27c0104c7893eb8424
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-25 14:53:50 +0100

    added eslint to enforce code style

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
+app.get('/', (req, res) => {
+  res.send('it works even some more')
+})
 
-app.get('/',(req,res)=>{
-    res.send('it works even some more')
-});
-
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000');
-});
-
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
