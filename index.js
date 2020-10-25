0f7f186e13b7035041f640ae85b1bc626e62096e
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-25 13:41:11 +0100

    added express configuration

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('it works')
+});
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000');
+});
+
