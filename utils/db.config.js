742d575ac0cf4488e1852b8369b5fcf02c6eca1c
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-26 23:50:19 +0100

    created connection to mongodb through mongoose

---
+++ utils/db.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/shopOne', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+  console.log('connected to mongodb')
+})
