1c8e95c30817fa3dfe0416c2438e8a6aac878c38
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-10-31 15:21:41 +0100

    added bcrpt for hashing submitted password

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const User = require('./models/User')
+
 require('./utils/db.config')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register', { message: null })
+})
+
+app.post('/register', async (req, res) => {
+  const user = new User(req.body)
+  await user.save()
+  console.log(user)
+  return res.render('register', { message: `Hi, ${user.name}, registration was successful` })
 })
 
 app.listen(3000, () => {
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    min: [2, 'Your name should be longer than 2 characters'],
+    max: [64, 'Your name cant be longer than 64 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    max: [128, 'Your email should not be longer than 128 characters'],
+    index: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+})
+
+// check userSchema password
+userSchema.pre('save', async function (next) {
+  // check if the password has not been modified
+  if (!this.isModified('password')) {
+    // call our next middleware function
+    next()
+  } else {
+    // if it has been modified encrypt password using bcrypt like below
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+  next()
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
