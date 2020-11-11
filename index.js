ad08a1ed0a4a3dbe39c9ccb80e5ea41ee8c50b60
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2020-11-11 23:01:57 +0100

    started working with sessions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const authRoutes = require('./routes/authRoutes')
 
@@ -8,12 +9,20 @@ require('./utils/db.config')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({
+  secret: 'e101d356ea852d134cfc1dcc723544ec7cbe4a65', // encrypt session data
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.set('view engine', 'ejs')
 
 app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`You have visited our site ${req.session.views} times`)
   res.render('index')
 })
 
